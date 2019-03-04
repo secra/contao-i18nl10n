222edb1552373633a08fb691dd53133ebd56ce37
Daniel Höring <1394078+c3nj1@users.noreply.github.com>
2019-03-04 16:04:58 +0100

    added a more safe way to call a language redirect

--- src/Hook/GetRootPageFromUrlHook.php
+++ src/Hook/GetRootPageFromUrlHook.php
@@ -3,6 +3,7 @@
 namespace Verstaerker\I18nl10nBundle\Hook;
 
 use Contao\Config;
+use Contao\Database;
 use Verstaerker\I18nl10nBundle\Classes\I18nl10n;
 
 /**
@@ -36,16 +37,36 @@ class GetRootPageFromUrlHook
         if (in_array($strLanguage, $accept_language)) {
             \Input::setGet('language', $strLanguage);
         } else {
-            switch (Config::get("i18nl10n_urlParam")) {
-                case 'parameter':
-                    $redirect = '/?language=en';
-                    break;
-                default:
-                    $redirect = '/en/index.html';
-            }
 
-            \Controller::redirect($redirect, 302);
-            exit;
+            $db = Database::getInstance();
+
+            $sql   = <<<SQL
+            SELECT c.*
+            FROM tl_page a
+            LEFT JOIN tl_page b ON a.id=b.pid
+            LEFT JOIN tl_page_i18nl10n c ON c.pid=b.id
+            WHERE a.dns = ?
+              and a.type = 'root'
+             and c.language = ?
+            ORDER BY b.sorting
+            LIMIT 1
+SQL;
+
+            $query = $db->prepare($sql)->execute($_SERVER["SERVER_NAME"], "en");
+
+            if ($row = $query->fetchAssoc()) {
+                $redirect = null;
+                switch (Config::get("i18nl10n_urlParam")) {
+                    case 'parameter':
+                        $redirect = "?language=en";
+                        break;
+                    default:
+                        $redirect = "en/index.html";
+                }
+
+                \Controller::redirect("/${row["alias"]}/${redirect}", 302);
+                exit;
+            }
         }
 
         return null;
