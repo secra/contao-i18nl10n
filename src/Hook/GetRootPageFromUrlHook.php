a394052f0e4ad8c5f116b15a056e122dd038b7ea
Daniel Höring <1394078+c3nj1@users.noreply.github.com>
2019-03-04 14:48:28 +0100

    fixed endless loop on non-german browsers

--- src/Hook/GetRootPageFromUrlHook.php
+++ src/Hook/GetRootPageFromUrlHook.php
@@ -2,13 +2,14 @@
 
 namespace Verstaerker\I18nl10nBundle\Hook;
 
+use Contao\Config;
 use Verstaerker\I18nl10nBundle\Classes\I18nl10n;
 
 /**
  * Class GetRootPageFromUrlHook
+ *
  * @package Verstaerker\I18nl10nBundle\Hook
  *
- * Translate root page to the "foreign" version, currently hardcoded to en/index
  * https://docs.contao.org/books/api/extensions/hooks/getRootPageFromUrl.html
  */
 class GetRootPageFromUrlHook
@@ -19,31 +20,34 @@ class GetRootPageFromUrlHook
      */
     public function getRootPageFromUrl()
     {
-		$blnDebug = true;
-		if($_COOKIE["dev"] == 'Markus'){
-			$blnDebug = true;
-		}
-		
-		$arrLanguages = I18nl10n::getInstance()->getLanguagesByDomain();
+        $arrLanguages = I18nl10n::getInstance()->getLanguagesByDomain();
 
         // If no root pages found, return
         if (!count($arrLanguages)) {
             return null;
         }
-		
-		// Get default language
-        $strLanguage  = $arrLanguages['default'];
-		
-		// find out if the browser likes German, if not - redirect to english homepage
-		$accept_language = \Environment::get('httpAcceptLanguage');
-		
-		if(in_array($strLanguage, $accept_language)) {	
-			\Input::setGet('language', $strLanguage);
-		} else {
-			\Controller::redirect('/en/index.html', 302);
-			exit; exit;
-		} 
-	
+
+        // Get default language
+        $strLanguage = $arrLanguages['default'];
+
+        // find out if the browser likes German, if not - redirect to english homepage
+        $accept_language = \Environment::get('httpAcceptLanguage') ?: array();
+
+        if (in_array($strLanguage, $accept_language)) {
+            \Input::setGet('language', $strLanguage);
+        } else {
+            switch (Config::get("i18nl10n_urlParam")) {
+                case 'parameter':
+                    $redirect = '/?language=en';
+                    break;
+                default:
+                    $redirect = '/en/index.html';
+            }
+
+            \Controller::redirect($redirect, 302);
+            exit;
+        }
+
         return null;
     }
 }
